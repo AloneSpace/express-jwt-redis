0aa287d210d85c98b01980533070cd7097b3063c
AloneSpace <jomcootle@gmail.com>
2021-07-21 19:04:24 +0700

    + Add dot package for .env file & import jwt service

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT || 3000;
+const dotenv = require('dotenv');
+dotenv.config();
+const PORT = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true, limit: '2mb'}))
 
 //* Set up router
 require('./routes/index')({express, app})
+
+//* Set up redis
+require('./services/jwt')
 
 //* Listen port
 app.listen(PORT, () => {
